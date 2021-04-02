a7fdecb6e8f72d3b02bab2268159fa6a309e6f51
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-04-02 12:30:09 +0300

    4. candicates.jsp - список кандидатов

--- src/main/java/ru/job4j/dreamjob/store/Store.java
+++ src/main/java/ru/job4j/dreamjob/store/Store.java
@@ -1,9 +1,7 @@
 package ru.job4j.dreamjob.store;
 
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.model.Post;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
@@ -12,18 +10,26 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Store {
     private static final Store INST = new Store();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     public Store() {
         posts.put(1, new Post(1, "Junior Java Job", "Need Junior", new Date()));
         posts.put(2, new Post(2, "Middle Java Job", "Need Middle", new Date()));
         posts.put(3, new Post(3, "Senior Java Job", "Need Senior", new Date()));
+        candidates.put(1, new Candidate(1, "Junior Java сandidate"));
+        candidates.put(2, new Candidate(2, "Middle Java сandidate"));
+        candidates.put(3, new Candidate(3, "Senior Java сandidate"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
