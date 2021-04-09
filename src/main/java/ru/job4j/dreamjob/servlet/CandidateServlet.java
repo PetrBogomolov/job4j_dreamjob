e7b625734d4de707bf1c6e379ba86c03bea417ce
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-04-09 07:55:06 +0300

    0. MVC в Servlet

--- src/main/java/ru/job4j/dreamjob/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/dreamjob/servlet/CandidateServlet.java
@@ -8,11 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CandidateSave extends HttpServlet {
+public class CandidateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("candidates", Store.instOf().findAllCandidates());
+        req.getRequestDispatcher("candidates.jsp").forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().saveCandidate(new Candidate(0, req.getParameter("name")));
-        resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
+        resp.sendRedirect(req.getContextPath() + "/candidates.do");
     }
 }
